fe4f829a3551b54aba254a58f66dce9172f53bc7
PaulMarv <mpsalako@gmail.com>
2025-04-16 00:19:49 +0100

    setup shacnui component and fonts

--- src/app/(website)/(home)/page.tsx
+++ src/app/(website)/(home)/page.tsx
@@ -1,9 +1,14 @@
+
 export default function Home() {
   return (
     <div>
-      <h1 className="text-[30px]">
-        ENYATA COMMUNITY TALENT DASHBOARD LANDING PAGE
-      </h1>
+      <div className="max-w-7xl p-5 mx-auto">
+        <h1 className="text-[29px] text-primary font-bold text-center">
+          ENYATA COMMUNITY TALENT DASHBOARD LANDING PAGE
+        </h1>
+        <p>/login for login page</p>
+        <p>/signup for signup page</p>
+      </div>
     </div>
   );
 }
