4913e2c2597940999a68d4017260da39d8eb0f7e
PaulMarv <mpsalako@gmail.com>
2025-04-15 18:07:41 +0100

    basic folder setup

--- src/app/(website)/(home)/page.tsx
+++ src/app/(website)/(home)/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div>
       <h1 className="text-[30px]">
-        ENYATA COMMUNITY TALENT DASHBOARD
+        ENYATA COMMUNITY TALENT DASHBOARD LANDING PAGE
       </h1>
     </div>
   );
