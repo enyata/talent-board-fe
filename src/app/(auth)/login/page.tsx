cdf551a10bdbb56a82463030c77721a3e50a6e43
PaulMarv <mpsalako@gmail.com>
2025-04-20 10:54:05 +0100

    adjustment on auth form

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,10 +1,17 @@
+'use client';
+import { motion } from "framer-motion";
 import React from "react";
 import Link from "next/link";
 import AuthForm from "../components/auth-form";
 
 const LoginPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center max-w-[360px] w-full">
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      className="flex flex-col items-center justify-center max-w-[360px] w-full"
+    >
       <h2 className="font-bold text-3xl">Login to your account</h2>
       <AuthForm action={"login"} />
       <p className="text-[14px] mt-[32px]">
@@ -14,7 +21,7 @@ const LoginPage = () => {
           Sign Up
         </Link>
       </p>
-    </div>
+    </motion.div>
   );
 };
 
