a9e3723732694f0ae98c7718a154d1c362ef03c9
PaulMarv <mpsalako@gmail.com>
2025-06-17 21:53:38 +0100

    cleanups

--- src/app/(auth)/auth/callback/page.tsx
+++ src/app/(auth)/auth/callback/page.tsx
@@ -1,30 +1,3 @@
-// import { env } from "@/lib/env";
-// import { redirect } from "next/navigation";
-
-// interface CallbackPageProps {
-//   searchParams: {
-//     access_token?: string;
-//     refresh_token?: string;
-//   };
-// }
-
-// export default function AuthCallbackPage({ searchParams }: CallbackPageProps) {
-//   const accessToken = searchParams.access_token;
-//   const refreshToken = searchParams.refresh_token;
-
-//   if (!accessToken || !refreshToken) {
-//     redirect("/login");
-//   }
-//   console.log('refresh token at callback', refreshToken)
-//   console.log('access token at callback', accessToken)
-
-//   if (env("appEnv") === 'local') {
-//     redirect(`/api/auth/finalize?access_token=${accessToken}&refresh_token=${refreshToken}`);
-//   } else {
-//     redirect(`/auth/finalize/client?access_token=${accessToken}&refresh_token=${refreshToken}`);
-//   }
-
-// }
 'use client'
 import { Suspense } from 'react';
 import AuthCallbackPageComponent from './callbackPageComponent';
