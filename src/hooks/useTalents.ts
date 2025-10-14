a125598238063515ac05e3ebe760cedf9f6590a7
PaulMarv <mpsalako@gmail.com>
2025-10-14 02:12:47 +0100

    switched query control from rhf to zustand for state

--- src/hooks/useTalents.ts
+++ src/hooks/useTalents.ts
@@ -1,29 +1,26 @@
 'use client';
 
 import { GET, POST } from '@/lib/requests';
+import { PaginatedData, TalentParams } from '@/types/talentParam.type';
+import { talentProp } from '@/types/user';
 
 interface UseTalentApiProps {
-    params?: {
-        limit?: number;
-        direction?: string;
-        cursor?: string;
-        sort?: string;
-        q?: string;
-        skills?: string;
-        experience?: string;
-        country?: string;
-        state?: string;
-    }
+    params?: TalentParams;
 }
 export const useTalentApi = () => {
-    const fetchAllTalents = async ({ params }: UseTalentApiProps) => {
-        const res = await GET('/api/v1/talents', params);
-        return res?.data ?? null;
-    }
-    const fetchSavedTalents = async ({ params }: UseTalentApiProps) => {
-        const res = await GET('/api/v1/talents/saved', params);
-        return res?.data ?? null;
-    }
+     const fetchAllTalents = async (
+    { params }: UseTalentApiProps
+  ): Promise<PaginatedData<talentProp> | null> => {
+    const res = await GET('/api/v1/talents', params);
+    return res?.data ?? null;
+  };
+
+  const fetchSavedTalents = async (
+    { params }: UseTalentApiProps
+  ): Promise<PaginatedData<talentProp> | null> => {
+    const res = await GET('/api/v1/talents/saved', params);
+    return res?.data ?? null;
+  };
     const fetchTalentById = async (id: string) => {
         const res = await GET(`/api/v1/talents/${id}`);
         return res?.data ?? null;
