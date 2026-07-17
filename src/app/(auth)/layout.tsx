69e67f1bb360a828c6d485ea39613e5803b970e0
muritz <mauricechisom275@gmail.com>
2026-07-17 14:34:01 +0100

    fix: auth page spacing

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { useEffect, useRef } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import TestimonyCarousel from './components/testimony-carousel'
@@ -8,12 +7,6 @@ import TestimonyCarousel from './components/testimony-carousel'
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="relative md:h-screen">
-      {/* <Link
-        href="/"
-        className="absolute md:top-[32px] top-[24px] md:left-[32px] left-[24px] text-[24px] font-semibold z-10 cursor-pointer"
-      >
-        Talentboard
-      </Link> */}
       <Link
         href={"/"}
         className="absolute md:top-[32px] top-[24px] md:left-[32px] left-[24px] z-10 flex items-center gap-[4px] md:gap-[10px]"
@@ -30,11 +23,11 @@ const AuthLayout = ({ children }: { children: React.ReactNode }) => {
       </Link>
 
       <div className="block md:flex md:min-h-screen overflow-y-scroll md:overflow-hidden">
-        <section className="w-full md:w-1/2 flex items-center justify-center px-4 h-[850px]">
+        <section className="w-full md:w-1/2 flex items-center justify-center px-4 h-[800px]">
           {children}
         </section>
 
-        <section className="w-full md:w-1/2 relative h-screen md:h-[unset]">
+        <section className="w-full md:w-1/2 relative h-screen md:h-[800px]">
           <Image
             src="/assets/images/auth-img.avif"
             alt="Auth image"
